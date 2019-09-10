1c008fba3947859080c215b47b1215740297d867
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-09-10 17:24:41 -0300

    Primeira aula de  console

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1, n2, resultado;
+
+            Console.Write("Informe o 1 numero - >");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2 numero - >");
+            n2 = int.Parse(Console.ReadLine());
+            resultado = n1 + n2;
+            Console.Write(resultado);
+            Console.ReadKey();
         }
     }
 }
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o livro a ser consultado:");
+            var numeroDoLivro = Console.ReadLine();
+            /* PARTE DO CODIGO QUE RECEBE AS INFORMAÇÕES */
+            if (numeroDoLivro == "89562")
+            {
+                Console.WriteLine("Livro Indisponivel!!!!!");
+                return;
+            } else    {
+                Console.WriteLine("Deseja alocar o livro" + ("(1)sim (2)não"));
+                var resposta = Console.ReadLine();
+                if(resposta == "1")
+                {
+                    Console.WriteLine("Livro alugado.");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
