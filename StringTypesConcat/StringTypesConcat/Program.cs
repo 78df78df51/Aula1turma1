1fed193fa6c1c49e9d766494e793a5d1e8ac79d3
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-09-11 17:15:46 -0300

    comutação

---
+++ StringTypesConcat/StringTypesConcat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringTypesConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = @"alguma coisa +  alguma infor here AMBEV";
+
+            var teste1 = Console.ReadLine();
+            var teste2 = Console.ReadLine();
+            var teste3 = teste1 +"\r\n"+ teste2;
+
+            Console.WriteLine(teste3);
+
+            var testeTemplate = @"Nome do usuario:{0} Idade:{1} Descrição:{2}";
+            var finishText = string.Format(testeTemplate
+                , Console.ReadLine()
+                , Console.ReadLine()
+                , Console.ReadLine());
+
+            Console.WriteLine(teste);
+            Console.ReadLine();
+        }
+    }
+}
