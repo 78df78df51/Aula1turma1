2c3ba36b4cec43925128f40ea1e12678dc423f2b
78df78df51 <55096189+78df78df51@users.noreply.github.com>
2019-09-15 14:48:49 -0300

    Add files via upload

---
+++ TestCasa1/TestCasa6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCasa6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeiroTexto = "'Entre restos de fezes e bananas, eu comeria a sua banana.'";
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine("Vou modificar o texto acima trocando a palavra 'banana' por 'Laranjas'");
+            Console.ReadKey();
+            primeiroTexto = primeiroTexto.Replace("banana", "Laranja");
+            Console.WriteLine(primeiroTexto);
+            Console.ReadKey();
+        }
+    }
+}
