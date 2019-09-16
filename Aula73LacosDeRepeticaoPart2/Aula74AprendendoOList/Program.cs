f14055710716f1e42f6e28e18709504c4407a5b3
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-09-16 17:23:54 -0300

    AULA SOBRE LISTA

---
+++ Aula73LacosDeRepeticaoPart2/Aula74AprendendoOList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula74AprendendoOList
+{
+    class Program
+    {
+        static List<string> minhaListaPulgmatica = new List<string>()
+            {
+                "Daniel",
+                "Irineu",
+                "Serilop"
+        };
+        static void Main(string[] args){
+            AdicionarItensALista();
+            ListaInformacoes();
+            Console.ReadKey();
+        }
+        /// <summary>
+        ///     Metodo que adiciona um item a lista
+        /// </summary>
+        private static void AdicionarItensALista()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um nome: ");
+            var nome = Console.ReadLine();
+            minhaListaPulgmatica.Add(nome);
+            Console.Clear();
+
+            Console.WriteLine("Deseja informar mais valores? sim(S) não (N)");
+            if (Console.ReadKey().KeyChar.ToString().ToLower() == "s")
+                AdicionarItensALista();
+            {
+
+            }
+        }
+        /// <summary>
+        /// Metodo que mostra a lista
+        /// </summary>
+        private static void ListaInformacoes()
+        {
+            //foreach (var item in minhaListaPulgmatica)
+            //Console.WriteLine(item);
+            Console.Clear();
+            Console.WriteLine("Nomes adicionados a lista");
+            foreach (var item in minhaListaPulgmatica)
+                Console.WriteLine($"Nome: {item} foi adicionado a lista");
+            
+        }
+    }
+}
