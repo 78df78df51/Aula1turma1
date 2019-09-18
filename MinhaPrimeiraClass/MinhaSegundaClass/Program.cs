289891a1c3f7b0397f7a682af0852c3f69e385ca
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-09-18 17:29:54 -0300

    AULA DE TERÇA 17/09 Orientaçao

---
+++ MinhaPrimeiraClass/MinhaSegundaClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinhaSegundaClass.Model;
+
+namespace MinhaSegundaClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestaDeFrutas = new List<Frutas>();
+            
+               minhaCestaDeFrutas.Add(new Frutas()
+               {
+                   nome = "Maça",
+                   Quantidade = 5
+                  
+               });
+            minhaCestaDeFrutas.Add(new Frutas()
+            {
+                nome = "Morango",
+                Quantidade = 2
+
+            });
+
+            minhaCestaDeFrutas.ForEach(i => Console.WriteLine($"Nome {i.nome} > Quant {i.Quantidade} "));
+            Console.ReadKey();
+        }
+    }
+}
