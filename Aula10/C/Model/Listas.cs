33b7bd019a4bd7638c9078e4f8c4a5f00ca5a900
Daniel Viegas Vieira <daniel.vieira@hbsis.com.br>
2019-09-23 08:19:29 -0300

    commit

---
+++ Aula10/C/Model/Listas.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C.Model
+{
+    class Listas
+    {
+        static void Main(string[] args)
+        {
+
+            var objetoPessoa = new Pessoa();
+
+            Console.WriteLine("Informe seu nome: ");
+            objetoPessoa.Nome = Console.ReadLine();
+
+            Console.WriteLine("informe sua idade: ");
+            objetoPessoa.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("sexo M ou F");
+            objetoPessoa.Sexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe sua altura: ");
+            objetoPessoa.Altura = double.Parse(Console.ReadLine());
+
+            cadastro2.Add(objetoPessoa);
+
+            Console.Clear();
+            cadastro2.ForEach(ob => Console.WriteLine($"Me chamo {objetoPessoa.Nome} possuo {ob.Idade}, sou do sexo {ob.Sexo} e tenho {ob.Altura}."));
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //List<int> numeros = new List<int>();
+            //Random rdm = new Random();
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    //gera um numero aleatorio de 0 a 100
+            //    int valorGeradoAleatoriamente = rdm.Next(100);
+            //    //verirfica se a lista não contem numeto gerado
+            //    bool ehRepetido = numeros.Contains(valorGeradoAleatoriamente);
+            //        if (!numeros.Contains(valorGeradoAleatoriamente))
+            //        {
+            //        //Adiciona o numero gerado pelo random (que ñ é repetido)
+            //        numeros.Add(valorGeradoAleatoriamente);
+            //        }
+            //        else
+            //    {
+            //        i--;
+            //    }
+
+        }
+            Console.ReadLine();
+        }
+
+    internal class Pessoa
+    {
+        public string Nome { get; internal set; }
+    }
+}
+}
---
+++ Aula10/C/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C.Model;
+
+namespace C
+{
+   public class Program
+    {
+        static List<Carro> listaCarro = new List<Carro>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 1 para inserir. ");
+            var opcao = Console.ReadLine();
+            var cadastro2 = new List<Carro>();
+
+            while (opcao == "1")
+            {
+                AdicionarLista();
+                Console.WriteLine("Digite 1 para inserir.");
+                opcao = Console.ReadLine();
+            }
+            listar();
+            Console.ReadKey();
+
+        }
+        public static void AdicionarLista()
+        {
+            var ObjetoCarro = new Carro();
+
+            Console.WriteLine("Informe a marca do seu carro ");
+            ObjetoCarro.Marca = Console.ReadLine();
+
+            Console.WriteLine("modelo: ");
+            ObjetoCarro.Modelo = Console.ReadLine();
+
+            Console.WriteLine("ano: ");
+            ObjetoCarro.Ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("valor: ");
+            ObjetoCarro.Valor = int.Parse(Console.ReadLine());
+
+            listaCarro.Add(ObjetoCarro);
+
+            Console.Clear();
+
+           
+        }
+        public static void listar()
+        {
+            listaCarro.ForEach(ob => Console.WriteLine($"Carro da marca: {ob.Marca} do modelo: {ob.Modelo} " +
+               $", ano: {ob.Ano} e valor de: {ob.Valor}."));           
+        }
+    }
+}
