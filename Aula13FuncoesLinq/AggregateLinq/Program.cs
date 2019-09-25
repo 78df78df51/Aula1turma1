b8aca534c729752f14c4b2f6e97f67d396e1f070
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-09-25 14:21:10 -0300

    exercicio em sala 25/09 - quarta

---
+++ Aula13FuncoesLinq/AggregateLinq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateLinq
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> listaDeNomes = new List<string>()
+			{
+				"Felipe",
+				"Giomar",
+				"Euricledson",
+				"Adagaberto",
+				"Ezebio",
+				"Irineudson"
+			};
+
+			var texto = listaDeNomes
+				//No aggregate temos o nome atula que estamos lendo
+				//e o proxim nome da lista
+				.Aggregate((nome1, nome2) => nome1 + ";\r\n " + nome2);
+				//No passo seguinte temos o nome  'próximo' vira o atual
+				//e pega o proximo da lista apos o atual
+			Console.WriteLine(texto);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Aula13FuncoesLinq/AvaregeLinq/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaregeLinq
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Vendas> vendas = new List<Vendas>()
+			{
+			new Vendas()
+			{
+				Produto = "XIAOMI Lite 6 quad Core 128 GB 32px frontal",
+				Quantidade = 8,
+				Valor = 1200.75
+			},
+			new Vendas()
+			{
+				Produto = "SLIM 3500 APP",
+				Quantidade = 5,
+				 Valor = 2800.96
+				
+			},
+			new Vendas()
+			{
+				Produto = "Motorola z3 edition project",
+				Quantidade = 1,
+				Valor = 1999.99
+			}
+			};
+			Console.WriteLine("Media de produtos vendidos este mês: ");
+			Console.WriteLine(vendas.Average(x => x.Quantidade));
+			Console.WriteLine("Média total de vendas neste mês em R$: ");
+			Console.WriteLine(vendas.
+				//Aqui realizamos o calculo de quatidade * valor = total de venda do produto
+				Average(x => (x.Quantidade * x.Valor)));
+				//Avarege divide o valor total pela quantidade de itens em nossa lista
+
+			Console.ReadKey();
+		}
+	}
+}
