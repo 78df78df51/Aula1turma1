58f1d6449d1e84c5162170276a784a48af05895c
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-10-04 17:22:25 -0300

    gdfcgff

---
+++ Aula22Sexta/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula22Sexta
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
