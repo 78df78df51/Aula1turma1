2691a8f9b8f04ab19942a77155dc5939c00c58ff
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-10-07 17:25:00 -0300

    AULA DE SEGUNDA

---
+++ Entity2/TesteEntity/Controller/PessoaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteEntity.Model;
+
+namespace TesteEntity.Controller
+{
+	public class PessoaController
+	{
+		EntityContextDB contextDB = new EntityContextDB();
+		public IQueryable<Pessoa>GetPessoas()
+		{
+			return contextDB.ListaDePessoa;
+		}
+		public void AddPessoa(Pessoa item)
+		{
+			contextDB.ListaDePessoa.Add(item);
+			contextDB.SaveChanges();
+		}
+	}
+}
