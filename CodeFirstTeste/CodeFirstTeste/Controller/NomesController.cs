41247845c24b4b89b1ab9788ed89ad4cf6f36fab
Daniel Viegas Vieira <daniel.vieira@hbsis.com.br>
2019-10-09 00:01:19 -0300

    de madrugada

---
+++ CodeFirstTeste/CodeFirstTeste/Controller/NomesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstTeste.Model;
+namespace CodeFirstTeste.Controller
+{
+	public class NomesController
+	{
+		NomeContextDB contextDB = new NomeContextDB();
+
+
+		public
+			IQueryable // Aqui temos nossa primeira Interface com a classe
+					   // IQueryable essa classe contem varioas funcionalidades
+					   // prontas para usar igual ao banco de dados como os selects
+			<NomePessoal> //Definimos o tipo que ira retornar 
+			GetPessoas() // Damos um nome ao nosso metodo
+		{
+			return contextDB.ListaDePessoa;//retornamos nossa lista de pessoas
+		}
+
+		/// <summary>
+		/// Metodo para adicionar Pessoa no banco de dados
+		/// </summary>
+		/// <param name="item">Item Pessoa</param>
+		public void AddPessoa(NomePessoal item)
+		{
+			contextDB //Nosso banco de dados 
+				.ListaDePessoa //nossa tabela Pessoa
+				.Add(item); //Adicionamos o item
+
+			contextDB.SaveChanges(); //Salvamos no banco
+		}
+	}
+}
---
+++ CodeFirstTeste/SistemasDeNomes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstTeste.Model;
+using CodeFirstTeste.Controller;
+
+namespace SistemasDeNomes
+{
+	class Program
+	{
+		
+			static NomesController pessoa = new NomesController();
+
+			static void Main(string[] args)
+			{
+				var escolha = int.MinValue;
+				while (escolha != 0)
+				{
+					Console.Clear();
+					Console.WriteLine("Menu sistema");
+					Console.WriteLine("1 - Listar usuários");
+					Console.WriteLine("2 - Cadrastrar usuário");
+					Console.WriteLine("0 - Sair");
+					var opcao = Console.ReadLine();
+					Console.Clear();
+					switch (opcao)
+					{
+						case "1":
+							SistemasDeNomes.Program.pessoa.GetPessoas().ToList<NomePessoal>()
+								.ForEach(x => Console.WriteLine($"nome:{x.Nome}"));
+
+							Console.WriteLine("Aperte qualquer tecla para continuar...");
+							Console.ReadKey();
+							Console.Clear();
+							break;
+						case "2":
+							Console.WriteLine("Digite um nome para adicionar a lista:");
+							pessoa.AddPessoa(
+					   new NomePessoal()
+					   {
+						   Nome = Console.ReadLine(),
+
+					   });
+
+							Console.WriteLine("Aperte qualquer tecla para continuar...");
+							Console.ReadKey();
+							Console.Clear();
+							break;
+						case "0":
+							Console.WriteLine("Saindo... até a próxima ");
+							Console.ReadKey();
+							break;
+					}
+				}
+			}
+		}
+	}
+
